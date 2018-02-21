0c7b2ffd4c3c529188fecdadeafd43a216315709
FT <FT@Yildirim>
2018-02-21 14:19:06 +0300

    Düzenlemeler yapılıyor.

---
+++ Hku/Hku/Hku.Core/AppConst/StaticParams.cs
@@ -0,0 +1,9 @@
+﻿namespace Arch.Core.AppConst
+{
+    public struct StaticParams
+    {
+        public const string EcryptionPass = "SeniSordumYildizlara";
+        public const string CaptchaSecretKey = "CaptchaSecretKey";
+        public const string ReptiliaDBConnstr = "Data Source=10.10.10.74;initial Catalog=REPTILIA;user id=sa;password=X99r31-./;MultipleActiveResultSets=True";
+    }
+}
