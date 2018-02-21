17b53ff3f12612e515652f89c26d4ecd5c2024ac
FT <FT@Yildirim>
2018-02-21 16:33:01 +0300

    StaticParams oluşturuldu

--- Hku/Hku/Hku.Core/AppConst/StaticParams.cs
+++ Hku/Hku/Hku.Core/AppConst/StaticParams.cs
@@ -3,7 +3,10 @@
     public struct StaticParams
     {
         public const string EcryptionPass = "SeniSordumYildizlara";
+
         public const string CaptchaSecretKey = "CaptchaSecretKey";
+
         public const string ReptiliaDBConnstr = "Data Source=10.10.10.74;initial Catalog=REPTILIA;user id=sa;password=X99r31-./;MultipleActiveResultSets=True";
+
     }
 }
